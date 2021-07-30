0c04321988719c719860b785589f4056af003324
Imaparadox <stevemarsh117@gmail.com>
2021-07-30 17:12:55 -0400

    Finished delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,18 +1,66 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Category, Product, ProductTag } = require('../../models');
 const { findAll } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
-  Category.findAll()
-  // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name'
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE category.id = )')]
+    ],
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+        include: {
+          model: Product,
+          attributes: ['username']
+        }
+      },
+      {
+        model: ProductTag,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'category_name',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE)')]
+    ]
+    // be sure to include its associated Products
+
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
@@ -21,10 +69,29 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  console.log('id', req.params.id);
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
 
 module.exports = router;
