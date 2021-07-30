ef67814c259800ebb5571b87d2b806d9c9e73cf8
Imaparadox <stevemarsh117@gmail.com>
2021-07-30 17:56:47 -0400

    Added DELETE route in Product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -127,6 +127,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  console.log('id', req.params.id);
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
